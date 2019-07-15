bbf8af0ea91b9d0a3c5ce66e38e47489f3742e53
Mikhail_Bakhirev <7074033@gmail.com>
2019-07-15 03:26:37 +0300

    validation, font-size

--- src/scripts/feedback.js
+++ src/scripts/feedback.js
@@ -3,7 +3,9 @@ const sendButton = document.querySelector('.btn--form');
 
 sendButton.addEventListener('click', function (event) {
   event.preventDefault();  
-  var data = {
+
+  if (validateForm(form)) {
+    var data = {
     email: form.elements.email.value,
     name: form.elements.name.value,
     tel: form.elements.tel.value,
@@ -11,6 +13,30 @@ sendButton.addEventListener('click', function (event) {
   }
   const xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
-  xhr.open('POST', 'robot.php');
+  xhr.open('POST', 'msg.php');
   xhr.send(JSON.stringify(data));
+  }
+
 });
+
+function validateForm(form) {
+  let valid = true;
+  if (!validateField(form.elements.name)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.email)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.tel)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.services)) {
+    valid = false;
+  }
+  return valid;
+}
+
+function validateField(field) {
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
+}
