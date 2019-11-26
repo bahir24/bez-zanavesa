a183adc84e05db19c2ed8e3c6dd40722a5c553a4
Mikhail_Bakhirev <7074033@gmail.com>
2019-11-26 05:50:04 +0300

    js start

--- src/scripts/booking.js
+++ src/scripts/booking.js
@@ -1,11 +1,14 @@
 const btnsBook = document.querySelectorAll('.btn--book');
+const messageBook = document.querySelector('.booking');
 console.log(btnsBook);
 for (i = 0; i<btnsBook.length; i++) {
   btnsBook[i].addEventListener('click', function (event){
     event.preventDefault();
-    
+    // let mainDiv = document.body;
+    document.body.className = "stop-scrolling";
+    messageBook.classList.add("booking--active");
   });
-}
+};
 // btnsBook.addEventListener('click', function (event) {
 // event.preventDefault();
 // });
